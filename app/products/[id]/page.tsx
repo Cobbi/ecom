633c8741223b688db206bf8a321f20d05f15e7da
cobbi <wherescobby@gmail.com>
2025-07-16 14:59:40 +0000

    minor updates

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,7 +1,20 @@
-export default function ProductDetailsPage() {
+import ProductDetails from "@/app/components/ProductDetails";
+import { productsData } from "@/app/data/productsData";
+import NotFoundPage from "@/app/not-found/page";
+
+export default function ProductDetailsPage({params}: {params: { id: string }}) {
+
+  const product = productsData.find((product) => product.id === params.id);
+  if (!product) {
+    return <NotFoundPage />;
+  }
+
+
   return (
     <div className="mx-auto max-w-6xl px-4 py-8">
-        <ProductDetailsPage />
+      <h3>{product.name}</h3>
+      <p>{product.description}</p>
+      <p className="text-primary text-lg font-semibold">${product.price}</p>
     </div>
   );
 }
